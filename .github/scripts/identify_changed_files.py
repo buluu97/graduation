4709fa20c579b70e3acd18040307e9bad86a798d
XixianLiang <51265902107@stu.ecnu.edu.cn>
2025-11-05 09:53:09 +0800

    automation: update translation scripts

--- .github/scripts/identify_changed_files.py
+++ .github/scripts/identify_changed_files.py
@@ -5,22 +5,19 @@ from pathlib import Path
 CONFIG_PATH = Path(".github/translation-list.yml")
 
 
-with open(CONFIG_PATH, "r") as f:
-    file_config_list = list(yaml.safe_load(f))
-    monitored = []
-    for item in file_config_list:
-        monitored.append(item["path"])
-
 # get changed file list in this commit
-changed_files = subprocess.check_output(
+changed_files = set(subprocess.check_output(
     ["git", "diff", "--name-only", "HEAD~1", "HEAD"],
     encoding="utf-8"
-).splitlines()
+).splitlines())
 
-# get intersection
-to_translate = [f for f in changed_files if f in monitored]
+to_translate = []
+with open(CONFIG_PATH, "r") as f:
+    file_config_list = list(yaml.safe_load(f))
+    for item in file_config_list:
+        if item["path"] in changed_files or item["reviewed_cn"] in changed_files:
+            to_translate.append(item["path"])
 
 if to_translate:
+    print("Files to be translated:")
     print("\n".join(to_translate))
-else:
-    pass # empty output
