fe0a3ad8646f9df1236e9b3734631db3db88019f
XixianLiang <51265902107@stu.ecnu.edu.cn>
2025-11-06 22:27:19 +0800

    update translation workflow

--- .github/scripts/identify_changed_files.py
+++ .github/scripts/identify_changed_files.py
@@ -12,12 +12,21 @@ changed_files = set(subprocess.check_output(
 ).splitlines())
 
 to_translate = []
-with open(CONFIG_PATH, "r") as f:
-    file_config_list = list(yaml.safe_load(f))
-    for item in file_config_list:
-        if item["path"] in changed_files or item["reviewed_cn"] in changed_files:
-            to_translate.append(item["path"])
+for f in changed_files:
+    if (".github/scripts" in f) or (".github/workflows/markdown-translation-update.yml" in changed_files):
+        to_translate = ["TRANSLATE ALL"]
+        break
+
+if not to_translate:
+    with open(CONFIG_PATH, "r") as f:
+        file_config_list = list(yaml.safe_load(f))
+        for item in file_config_list:
+            if item["path"] in changed_files or item["reviewed_cn"] in changed_files:
+                to_translate.append(item["path"])
 
 if to_translate:
     print("Files to be translated:")
     print("\n".join(to_translate))
+
+with open("changed_files.txt", "w", encoding="utf-8") as f:
+    f.write("\n".join(to_translate))
