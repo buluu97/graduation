cb2533b07208171a8dd9724795abd33270ee7f4c
XixianLiang <51265902107@stu.ecnu.edu.cn>
2026-01-25 21:13:21 +0800

    update example

--- omninotes_sample.py
+++ omninotes_sample.py
@@ -1,25 +1,10 @@
 import unittest
-import uiautomator2 as u2
-
-from time import sleep
-from kea2 import precondition, prob, KeaTestRunner, Options, keaTestLoader, invariant
-
-
-import unittest
 import random
 import uiautomator2 as u2
 from uuid import uuid4
 from kea2 import precondition, prob, max_tries
 
 from kea2 import state   # stateful testing
-
-
-URL = "https://github.com/federicoiosue/Omni-Notes/releases/download/6.2.0_alpha/OmniNotes-alphaRelease-6.2.0.apk"
-FALL_BACK_URL = "https://gitee.com/XixianLiang/Kea2/raw/main/omninotes.apk"
-PACKAGE_NAME = "it.feio.android.omninotes.alpha"
-FILE_NAME = "omninotes.apk"
-
-
 state["notes"] = list()
 
 def get_random_text():
@@ -34,7 +19,7 @@ class TestOmniNotes(unittest.TestCase):
         """
         cls.d.settings["wait_timeout"] = 5.0
         cls.d.settings["operation_delay"] = (0, 1.0)
-        cls.d.app_clear(PACKAGE_NAME)
+        cls.d.app_clear("it.feio.android.omninotes.alpha")
 
     @prob(0.3)
     @precondition(
@@ -88,61 +73,3 @@ class TestOmniNotes(unittest.TestCase):
         if self.d(resourceId="it.feio.android.omninotes.alpha:id/done").exists:
             self.d(resourceId="it.feio.android.omninotes.alpha:id/done").click()
             
-    @invariant
-    def search_button_and_search_input_box_should_not_exists_at_the_same_time(self):
-        """Search input box and search button should not exists at the same time
-        """
-        search_input_box_exists = self.d(resourceId="it.feio.android.omninotes.alpha:id/search_src_text").exists
-        serach_button_exists = self.d(resourceId="it.feio.android.omninotes.alpha:id/menu_search").exists
-        if search_input_box_exists or serach_button_exists:
-            assert search_input_box_exists ^ serach_button_exists
-            
-
-
-# Download Utils
-def download_omninotes():
-    import socket
-    socket.setdefaulttimeout(30)
-    try:
-        import urllib.request
-        urllib.request.urlretrieve(URL, FILE_NAME)
-    except Exception as e:
-        print(f"[WARN] Download from {URL} failed: {e}. Try to download from fallback URL {FALL_BACK_URL}", flush=True)
-        try:
-            urllib.request.urlretrieve(FALL_BACK_URL, FILE_NAME)
-        except Exception as e2:
-            print(f"[ERROR] Download from fallback URL {FALL_BACK_URL} also failed: {e2}", flush=True)
-            raise e2
-
-
-def check_installation(serial=None):
-    import os
-    from pathlib import Path
-    
-    d = u2.connect(serial)
-    # automatically install omni-notes
-    if PACKAGE_NAME not in d.app_list():
-        if not os.path.exists(Path(".") / FILE_NAME):
-            print(f"[INFO] omninote.apk not exists. Downloading from {URL}", flush=True)
-            download_omninotes()
-        print("[INFO] Installing omninotes.", flush=True)
-        d.app_install(FILE_NAME)
-    d.stop_uiautomator()
-
-
-if __name__ == "__main__":
-    check_installation(serial=None)
-    KeaTestRunner.setOptions(
-        Options(
-            driverName="d",
-            packageNames=[PACKAGE_NAME],
-            # serial="emulator-5554",   # specify the serial
-            maxStep=50,
-            profile_period=10,
-            take_screenshots=True,  # whether to take screenshots, default is False
-            # running_mins=10,  # specify the maximal running time in minutes, default value is 10m
-            # throttle=200,   # specify the throttle in milliseconds, default value is 200ms
-            agent="u2"  # 'native' for running the vanilla Fastbot, 'u2' for running Kea2
-        )
-    )
-    unittest.main(testRunner=KeaTestRunner, testLoader=keaTestLoader)
